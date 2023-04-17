0af7634571a7aed795ca039a2bc0f1b06c415ff6
GaryAnalysis <garyanalysis@gmail.com>
2023-04-17 20:04:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ import os
 import csv
 from pathlib import Path
 
+"""
+You will need to set the following environment variables:
+ - SPOTIPY_CLIENT_ID (This is your client ID, provided by Spotify)
+ - SPOTIPY_CLIENT_SECRET (This is your client secret, provided by Spotify)
+ - SPOTIPY_USER (This is your Spotify username)
+ - SPOTIPY_REDIRECT_URI (Redirect URI, e.g. http://localhost:8888/callback)
+"""
+
 # You can find your light's IP by running "find_ip.py"
 wiz_ip = "IP"
 
